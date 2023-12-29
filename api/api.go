de103347bc2d88a1919806d7a96221575b3b58b2
yohan <nogeek.lol@gmail.com>
2023-12-29 14:44:19 +0100

    correction affichage des ports utilisés et maj du README.md

--- api/api.go
+++ api/api.go
@@ -2,30 +2,34 @@ package api
 
 import (
 	"encoding/json"
+	_ "encoding/json"
 	"fmt"
 	"log"
+	sim "metrosim/internal/simulation"
 	"net/http"
 	"time"
-	sim "metrosim/internal/simulation"
 )
 
 func StartAPI(sim *sim.Simulation) {
 	mux := http.NewServeMux()
-
+	port := "12000"
 	station := func(w http.ResponseWriter, r *http.Request) {
 		msg, _ := json.Marshal(sim.Print())
 		fmt.Fprintf(w, "%s", msg)
 	}
 
 	mux.HandleFunc("/sim", station)
+	mux.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "OK")
+	})
 
 	s := &http.Server{
-		Addr:           ":12000",
+		Addr:           ":" + "12000",
 		Handler:        mux,
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20}
 
-	log.Println("Listening on localhost:8080")
+	log.Println(fmt.Sprintf("Listening on localhost:%s", port))
 	log.Fatal(s.ListenAndServe())
 }
